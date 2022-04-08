f1abaac2ee3593b518757a0262dfad601f6b4bf4
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-08 12:40:38 -0400

    added country spinner

---
+++ app/src/main/java/com/nirmalbhetwal/vacationplanner/Country.java
@@ -0,0 +1,24 @@
+package com.nirmalbhetwal.vacationplanner;
+
+import java.io.Serializable;
+
+public class Country implements Serializable {
+    private String name;
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- app/src/main/java/com/nirmalbhetwal/vacationplanner/MainActivity.java
+++ app/src/main/java/com/nirmalbhetwal/vacationplanner/MainActivity.java
@@ -3,12 +3,42 @@ package com.nirmalbhetwal.vacationplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner countrySpinner;
+    ArrayList<Country> countries = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countrySpinner = (Spinner) findViewById(R.id.spCountries);
+        fillData();
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, countries);
+        countrySpinner.setAdapter(adapter);
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("sdf',", "sdfs");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void fillData() {
+        countries.add(new Country("Nepal"));
+        countries.add(new Country("China"));
     }
 }
